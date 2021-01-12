25a7dc481d881773d36d0d7cfe2239d34714ea76
Daniel <daniel.castelline@gmail.com>
2021-01-11 23:25:35 -0600

    removed index.html from folder, attempts to match time of day to the hours of the planner

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,3 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 // Begin function
 $("document").ready(function() {
    
@@ -52,10 +38,17 @@ $("document").ready(function() {
         var value = $(this).siblings(".description").val();
         var time = $(this).siblings(".time").text();
         localStorage.setItem(time, value);
-        $("this").siblings(".description").textContent(localStorage.value);
+        $("this").siblings(".description").text(localStorage.value);
     });
 
+    // Tracking current hour(h)
+    h = currentDate.h;
+        console.log(h);
+    t = $(".time").text();
+        console.log(t);
+        
+
     // Background colors
-    
+    // if (h < )
 
 });
